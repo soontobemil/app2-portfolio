cd8bffc9252f3db525bf6a713c5af31e507cc37b
Jay Sim <yourthought2020@gmail.com>
2023-08-24 15:41:11 -0700

    Add col3 and col4 for title, description, images, and url

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 
 st.write(mainContent)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[source code]({row['url']})")
