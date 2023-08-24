54c4cc9217c9d018fee099da5dfffd385780ae88
Jay Sim <yourthought2020@gmail.com>
2023-08-24 15:09:44 -0700

    Chnage to use triple quotation marks

--- main.py
+++ main.py
@@ -9,27 +9,13 @@ with col1:
 
 with col2:
     st.title("Jay Sim")
-    content = "Jay Sim, based in Vancouver, British Columbia, is a certified DevOps engineer " \
-              "with proficiency in cloud platforms including AWS, Azure, GCP, and Exoscale. " \
-              "He boasts an extensive programming background with languages like Python, JavaScript, and Bash scripting. " \
-              "Jay is adept in leveraging DevOps tools like Kubernetes, Terraform, and Prometheus. His experience spans " \
-              "orchestrating CI/CD pipelines with GitLab, managing AWS resources, and setting up comprehensive monitoring systems. " \
-              "He's also previously automated sales reports for multi-million custom orders in a sales role. " \
-              "A graduate with distinction from Capilano University in Tourism Management, " \
-              "Jay also completed The Web Developer Bootcamp in 2022, producing a full-stack web application project. Fluent in both English and Korean, " \
-              "Jay's diverse skill set is underscored by certifications such as AWS Solutions Architect, Terraform Associate, and Certified Kubernetes Administrator."
+    content = """
+    Devops Engineer
+    """
     st.info(content)
 
-mainContent = "Lorem Ipsum is simply dummy text of the printing and " \
-              "typesetting industry. Lorem Ipsum has been the " \
-              "industry's standard dummy text ever since the 1500s, " \
-              "when an unknown printer took a galley of type and scrambled " \
-              "it to make a type specimen book. It has survived not only five " \
-              "centuries, but also the leap into electronic typesetting, " \
-              "remaining essentially unchanged. It was popularised in the " \
-              "1960s with the release of Letraset sheets containing Lorem " \
-              "Ipsum passages, and more recently with desktop publishing " \
-              "software like Aldus PageMaker " \
-              "including versions of Lorem Ipsum."
+mainContent = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+"""
 
 st.write(mainContent)
