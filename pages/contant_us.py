6884742038889b7f73fd28d5a32df26994378ebf
Jay Sim <yourthought2020@gmail.com>
2023-08-25 19:40:16 -0700

    Configure basic text areas and button

--- pages/contant_us.py
+++ pages/contant_us.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.header("Contact me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Enter your email")
+    message = st.text_area("Your message here")
+    button = st.form_submit_button("Submit")
+    if button:
+        print("I was pressed!")
