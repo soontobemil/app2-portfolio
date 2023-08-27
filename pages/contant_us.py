ddbcecd9801bf581a9750a4e89cf7a311422085d
Jay Sim <yourthought2020@gmail.com>
2023-08-26 20:56:08 -0700

    Export send_email as a function

--- pages/contant_us.py
+++ pages/contant_us.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Enter your email")
     message = st.text_area("Your message here")
+    message = message + "\n" + user_email
     button = st.form_submit_button("Submit")
     if button:
-        print("I was pressed!")
+        send_email(message)
