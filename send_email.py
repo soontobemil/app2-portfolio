143b367c7607588cdbf69489ef0b7720fa47cb62
Jay Sim <yourthought2020@gmail.com>
2023-08-27 11:03:16 -0700

    Import OS method to secure password

--- send_email.py
+++ send_email.py
@@ -1,14 +1,14 @@
 import smtplib
 import ssl
 import certifi
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "krjayvancouver@gmail.com"
-    password = ""
+    password = os.getenv("PASSWORD")
 
     receiver = "krjayvancouver@gmail.com"
     context = ssl.create_default_context(cafile=certifi.where())
