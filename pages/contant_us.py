3b1061dd55718a8f986398ecc899ba2140be8b61
Jay Sim <yourthought2020@gmail.com>
2023-08-26 21:00:21 -0700

    Adjust components

--- pages/contant_us.py
+++ pages/contant_us.py
@@ -5,8 +5,13 @@ st.header("Contact me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Enter your email")
-    message = st.text_area("Your message here")
-    message = message + "\n" + user_email
+    raw_message = st.text_area("Your message here")
+    message = f"""\
+Subject: New email from {user_email}
+From: {user_email}
+Content: {raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
         send_email(message)
+        st.info("Your email was sent successfully")
